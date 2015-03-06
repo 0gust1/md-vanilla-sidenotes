e0bb4a4864f92fb9b6a58b99321722455b981cac
0gust1 <augustin.chassine@gmail.com>
2015-03-06 23:30:43 +0100

    Livereload

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+
+function startExpress() {
+
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')());
+    app.use(express.static(__dirname));
+    app.listen(4000);
+}
+
+var lr;
+
+function startLivereload() {
+
+  lr = require('tiny-lr')();
+  lr.listen(35729);
+}
+
+function notifyLivereload(event) {
+
+  // `gulp.watch()` events provide an absolute path
+  // so we need to make it relative to the server root
+  var fileName = require('path').relative(__dirname, event.path);
+  lr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+// watch files for changes and reload
+
+gulp.task('default', function() {
+    startExpress();
+    startLivereload();
+    gulp.watch(['*.html', 'test/styles/**/*.css', 'src/**/*.js'], {cwd: 'test'}, notifyLivereload);
+});
+
+gulp.task('build',function(){
+    console.log('build task');
+});
